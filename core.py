78364a50846e6af0518cb905387c448175357302
MuspiMerol <wjj123345567@iCloud.com>
2022-02-15 21:54:58 +0800

    implemented several helpers in core.py

---
+++ core.py
@@ -0,0 +1,56 @@
+import ctypes
+import pyglet
+import glooey
+
+if "initialize":
+    assets_dir = "./assets"
+    pyglet.resource.path.append(assets_dir)
+    ctypes.windll.user32.SetProcessDPIAware(2)
+
+
+def get_block(size, *color):
+    file_name = f"{size}_{'_'.join(map(str, color))}.png"
+    try:
+        return pyglet.resource.texture(file_name)
+    except pyglet.resource.ResourceNotFoundException:
+        import numpy as np, cv2
+        match color:
+            case (k, ):
+                arr = np.full((size, size), k)
+            case (l, a):
+                arr = np.full((size, size, 4), (l, l, l, a))
+            case (r, g, b):
+                arr = np.full((size, size, 3), (r, g, b))
+            case (r, g, b, a):
+                arr = np.full((size, size, 4), (r, g, b, a))
+            case _:
+                raise TypeError(*color)
+        cv2.imwrite(f"{assets_dir}/{file_name}", arr)
+        pyglet.resource.reindex()
+        return pyglet.resource.texture(file_name)
+
+
+def get_bgd(inner, outer=None, *, size=1):
+    class _(glooey.Background):
+        custom_center = get_block(size, *inner)
+        if outer is not None:
+            block_outer = get_block(size, *outer)
+            custom_top = block_outer
+            custom_bottom = block_outer
+            custom_left = block_outer
+            custom_right = block_outer
+            custom_top_left = block_outer
+            custom_top_right = block_outer
+            custom_bottom_left = block_outer
+            custom_bottom_right = block_outer
+
+    return _
+
+
+def get_triplet(*color):
+    return (
+        get_bgd((*color, 10)),
+        get_bgd((*color, 10), (*color, 30)),
+        get_bgd((*color, 30), (*color, 90)),
+    )
+
--- learning_glooey.py
+++ learning_glooey.py
@@ -1,51 +1,40 @@
-import ctypes
-import pyglet
-import glooey
+from core import *
 
-ctypes.windll.user32.SetProcessDPIAware(2)
-pyglet.resource.path.append("./assets")
-style = pyglet.window.Window.WINDOW_STYLE_TRANSPARENT
+# style = pyglet.window.Window.WINDOW_STYLE_TRANSPARENT
+style = None
 
 ui = glooey.Gui(
     window := pyglet.window.Window(480, 360, resizable=True, style=style),
     batch=(batch := pyglet.graphics.Batch()),
-    group=(group := pyglet.graphics.Group())
+    group=(group := pyglet.graphics.Group()),
 )
 
-
-class Border(glooey.Background):
-    custom_center = pyglet.resource.texture('center.png')
-    custom_top = pyglet.resource.texture('top.png')
-    custom_bottom = pyglet.resource.texture('bottom.png')
-    custom_left = pyglet.resource.texture('left.png')
-    custom_right = pyglet.resource.texture('right.png')
-    custom_top_left = pyglet.resource.image('top_left.png')
-    custom_top_right = pyglet.resource.image('top_right.png')
-    custom_bottom_left = pyglet.resource.image('bottom_left.png')
-    custom_bottom_right = pyglet.resource.image('bottom_right.png')
+ui.add(get_bgd((255,))())
 
 
-ui.add_back(Border())
-
-
-class Button(glooey.Button):
+class BlackOnWhiteButton(glooey.Button):
     class Foreground(glooey.Label):
         custom_font_name = "JetBrains Mono"
         custom_font_size = 10
-        custom_color = (255, 155, 55, 255)
+        custom_color = (0, 0, 0, 255)
         custom_alignment = "center"
 
-    # class Base(glooey.Image):
-    #     custom_image = pyglet.resource.image("base.png")
+    Base, Over, Down = get_triplet(0)
 
-    class Over(glooey.Image):
-        custom_image = pyglet.resource.image("over.png")
+    def on_mouse_enter(self, x, y):
+        super().on_mouse_enter(x, y)
 
-    class Down(glooey.Image):
-        custom_image = pyglet.resource.image("down.png")
+    def __init__(self, *args, **kwargs):
+        glooey.Button.__init__(self, *args, **kwargs)
+        self.set_size_hint(100, 50)
+        w, h = self.get_foreground().do_claim()
+        self.set_size_hint(w + h * 2, h * 3)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        print(self.get_foreground().get_claimed_size())
 
 
-ui.add(Button("click me"))
+ui.add(BlackOnWhiteButton("click me"))
 
 if __name__ == '__main__':
     pyglet.app.run()
